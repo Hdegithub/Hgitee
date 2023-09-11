c6aa477075b181714dbe280b3f60ae7d023a1811
liaohao <whitealbum17@outlook.com>
2023-09-11 17:08:15 +0800

    阶段项目 引入lockback 查询新闻列表 根据id查询带出评论 模糊查询

--- News/src/main/java/com/geekaca/news/mapper/NewsMapper.java
+++ News/src/main/java/com/geekaca/news/mapper/NewsMapper.java
@@ -3,12 +3,15 @@ package com.geekaca.news.mapper;
 import com.geekaca.news.domain.News;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
 * @author kanata
 * @description 针对表【tb_news】的数据库操作Mapper
 * @createDate 2023-09-08 15:24:32
 * @Entity com.geekaca.news.domain.News
 */
+// TODO:添加@Mapper
 @Mapper
 public interface NewsMapper {
 
@@ -23,5 +26,14 @@ public interface NewsMapper {
     int updateByPrimaryKeySelective(News record);
 
     int updateByPrimaryKey(News record);
+
+    // 前台页面 新闻列表查询 泛型约束
+    List<News> selectAll();
+
+    News selectById(Long id);
+
+    //模糊查询 根据
+    List<News> selectByName(News news);
+
 
 }
