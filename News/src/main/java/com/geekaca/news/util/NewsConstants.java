b9627b21ac36bc84dfe3a5d4361ed8d113c6502a
liaohao <whitealbum17@outlook.com>
2023-09-15 16:03:25 +0800

    test 冲突

--- News/src/main/java/com/geekaca/news/util/NewsConstants.java
+++
@@ -1,7 +0,0 @@
-package com.geekaca.news.util;
-
-public class NewsConstants {
-    public static final String UPLOAD_PATH = "D:\\dev\\tmp\\upload";
-    public static final int RESULT_OK = 200;
-    public static final int RESULT_FAIL = 500;
-}
