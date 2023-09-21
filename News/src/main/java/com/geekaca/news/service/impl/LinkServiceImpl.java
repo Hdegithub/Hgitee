57f0fd5b82f6ea5906e68fa9b8d685a020c8156a
hejianbo <2948426785@qq.com>
2023-09-21 13:46:49 +0800

    修改版、

--- News/src/main/java/com/geekaca/news/service/impl/LinkServiceImpl.java
+++ News/src/main/java/com/geekaca/news/service/impl/LinkServiceImpl.java
@@ -3,7 +3,9 @@ package com.geekaca.news.service.impl;
 import com.geekaca.news.mapper.LinkMapper;
 import com.geekaca.news.service.LinkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LinkServiceImpl implements LinkService {
     @Autowired
     private LinkMapper linkMapper;
