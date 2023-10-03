4682e2e4e36d900991be2a7c5d35be26e5c6711a
liaohao <whitealbum17@outlook.com>
2023-10-03 15:35:59 +0800

    新闻项目 拦截器

--- News/src/main/java/com/geekaca/news/config/WebConfig.java
+++ News/src/main/java/com/geekaca/news/config/WebConfig.java
@@ -1,12 +1,23 @@
 package com.geekaca.news.config;
 
 import com.geekaca.news.utils.NewsConstans;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AdminLoginInterceptor adminLoginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 添加一个拦截器，拦截以/admin为前缀的url路径
+        registry.addInterceptor(adminLoginInterceptor).addPathPatterns("/admin/**").excludePathPatterns("/admin/login").excludePathPatterns("/admin/dist/**").excludePathPatterns("/admin/plugins/**");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**").addResourceLocations("file:" + NewsConstans.UPLOAD_PATH);
